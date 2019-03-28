229de3a300618325683875eb3657565711945b6e
Rafal Pijet <stronglopez@wp.pl>
2019-03-28 15:36:05 +0100

    Adding example from 25.5 task

--- server.js
+++ server.js
@@ -3,8 +3,13 @@ var app = express();
 
 app.use(express.static('assets'));
 
-app.get('/', function (req, res) {
-    res.sendFile('/index.html');
+app.use(function (req, res, next) {
+    console.log('Hej, jestem pośrednikiem między żądaniem a odpowiedzią!');
+    next();
+});
+
+app.get('/get', function (req, res) {
+    res.send('Hello World!');
 });
 
 app.get('/userform', function (req, res) {
@@ -17,7 +22,6 @@ app.get('/userform', function (req, res) {
 
 const server = app.listen(3000, 'localhost', function () {
     var host = server.address().address;
-    console.log(host);
     var port = server.address().port;
 
     console.log('Przykładowa aplikacja nasłuchuje na http://' + host + ':' + port);
